e148f3e86525a56a75a44fa3e86f6530e172ec55
Thomas Moser <thomas.moser@bmglabtech.com>
2023-03-02 14:46:50 +0100

    Debug prod activities fetch data

--- pages/activities.js
+++ pages/activities.js
@@ -48,10 +48,23 @@ function Activities() {
                     <h1 className='text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl text-white py-5 font-bold'>What I play</h1>
                     <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
                     <Player/>
+                    <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
+                    <Player/>
+                    <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
+                    <Player/>
+                    <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
+                    <Player/>
+                    <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
+                    <Player/>
+                    <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
+                    <Player/>
+                    <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
+                    <Player/>
+
 
                     
                     <p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>The games I recently played.</p>
-                    <Games/>
+                    {/* <Games/> */}
                     <h1 className='text-center mt-24 text-xl sm:text-2xl md:text-3xl lg:text-4xl text-white py-5 font-bold'>{"What I'm currently listening to."}</h1>
                     
                     {/*<p className='text-md my-4 text-center font-bold text-gray-400 sm:text-xl'>My status.</p>
