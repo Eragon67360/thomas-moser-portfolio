614762efb917e0b9172cb47a9eb81f2fb4121757
Eragon67360 <thomas-moser@orange.fr>
2024-06-05 22:05:36 +0200

    fix(next-sitemap.config.js): update siteUrl to use 'https://www.thomasmoserdev.com' as the default value feat(next-sitemap.config.js): add exclusion rule for '/api/*' paths in sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://thomasmoserdev.com',
+    siteUrl: process.env.SITE_URL || 'https://www.thomasmoserdev.com',
     generateRobotsTxt: true,
+    exclude: ['/api/*']
+
   }
